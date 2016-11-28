c41890044c376de9bdcccbc199cda8cb1338cc61
Aleksandar Bosnjak <a.bosnjak@levi9.com>
2016-11-28 13:48:44 +0100

    Added yeoman generated angular app.

---
+++ InsuranceCompany/src/InsuranceCompany/wwwroot/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name insuranceCompanyApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the insuranceCompanyApp
+ */
+angular.module('insuranceCompanyApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ InsuranceCompany/src/InsuranceCompany/wwwroot/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name insuranceCompanyApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the insuranceCompanyApp
+ */
+angular.module('insuranceCompanyApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
