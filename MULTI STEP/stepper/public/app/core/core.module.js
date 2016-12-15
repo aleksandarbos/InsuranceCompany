48be8768ddd82a4a76d1e588651458b756ed97ff
divljiboy <ivan93.ns@hotmail.com>
2016-12-15 18:37:15 +0100

    Drugi slajd lok

--- MULTI STEP/stepper/public/app/core/core.module.js
+++ MULTI STEP/stepper/public/app/core/core.module.js
@@ -28,9 +28,16 @@ var translationsEN = {
   DAYS: 'Number of days',
   CANCEL: 'CANCEL',
   NEXT: 'NEXT',
-  CHOOSE:'CHoose your insurance',
+  PREVIOUS:'PREVIOUS',
+  CHOOSE:'Choose your insurance',
   BUTTON_LANG_SRB: 'Serbian',
-  BUTTON_LANG_EN: 'English'
+  BUTTON_LANG_EN: 'English',
+  OFFER:'Package offer',
+  PACKAGE:'Choose package',
+  PRICE:'Price',
+  PACKAGE1:'Standard: Complete medical assistance is defined standard package cover.The cost of transport to the hospital, treatment and hospitalization. Medical and orthopedic equipment overwritten by a doctor.Treatment of acute toothache amounting to 150 €. Back in the patient'+'s home country in the amount of up to € 2,000.',
+  PACKAGE2:'Comfort: Complete medical assistance is defined comfort package cover. The cost of transport to the hospital, treatment and hospitalization. Medical and orthopedic equipment overwritten by a doctor.Treatment of acute toothache of up to € 300. Back patient'+'s home of up to € 7,000.Loss or theft of luggage up to 100 €. Translation service.',
+  PACKAGE3:'Exclusive: Complete medical assistance is defined exclusive package cover.The cost of transport to the hospital, treatment and hospitalization. Medical and orthopedic equipment overwritten by a doctor.Treatment of acute toothache of up to € 300. Back patient'+'s home of up to € 10,000.Loss or theft of baggage amounting to 200 €.Translation service. Return of children who have lost control (air ticket).Finding a lawyer at the expense of the insured.',
 };
  
 var translationsSRB= {
@@ -46,11 +53,18 @@ var translationsSRB= {
   BUISNESS: 'Poslovni put',
   STATE: 'Drzava',
   DAYS: 'Broj dana',
-  CANCEL: 'Odustani',
+  CANCEL: 'ODUSTANI',
   NEXT: 'DALJE',
-  CHOOSE:'Odabir putovanja'
+  PREVIOUS:'NAZAD',
+  CHOOSE:'Odabir putovanja',
   BUTTON_LANG_SRB: 'Srpski',
-  BUTTON_LANG_EN: 'Engleski'
+  BUTTON_LANG_EN: 'Engleski',
+  OFFER:'Ponuda paketa',
+  PACKAGE:'Izaberite paket',
+  PRICE:'Cena',
+  PACKAGE1:'Standard:Kompletna medicinska asistencija definisana standard paketom pokrića. Troškovi prevoza do bolnice, lečenja i hospitalizacije. Medicinska i ortopedska oprema prepisana od lekara. Lečenje akutne zubobolje u visini do 150€. Povratak pacijenta u domovinu u visini do 2.000€.',
+  PACKAGE2:'Komfort: Kompletna medicinska asistencija definisana komfort paketom pokrića. Troškovi prevoza do bolnice, lečenja i hospitalizacije. Medicinska i ortopedska oprema prepisana od lekara. Lečenje akutne zubobolje u visini do 300€. Povratak pacijenta u domovinu u visini do 7.000€. Gubitak ili krađa prtljaga u visini do 100€. Prevodilačke usluge.',
+  PACKAGE3:'Ekskluziv: Kompletna medicinska asistencija definisana ekskluziv paketom pokrića. Troškovi prevoza do bolnice, lečenja i hospitalizacije. Medicinska i ortopedska oprema prepisana od lekara. Lečenje akutne zubobolje u visini do 300€. Povratak pacijenta u domovinu u visini do 10.000€. Gubitak ili krađa prtljaga u visini do 200€. Prevodilačke usluge. Povratak dece koja su ostala bez nadzora (avionska karta). Pronalaženje advokata na račun osiguranika.',
 };
 
 
