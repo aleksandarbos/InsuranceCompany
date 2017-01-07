9cdfeaaf5ce710700cb727d1e8ef3552783f6253
aleksandarbos <aleksandarbos.ns@gmail.com>
2017-01-07 19:59:43 +0100

    Deleted unnecessary subprojects.

--- MULTI STEP/stepper/public/app/index.module.js
+++
@@ -1,7 +0,0 @@
-(function() {
-  'use strict';
-
- angular
-    .module('angularMaterialKitchenSink', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'ui.router', 'ngMaterial','ngMdIcons', 'toastr']);
-
-})();
